05b1c77a156739a70f31fa86a98d81238624ed1c
mazen <mazenchouchene81@gmail.com>
2024-06-25 13:15:57 +0200

    data ingestion notebook added

---
+++ main.py
@@ -0,0 +1,13 @@
+from text_summarize.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
+
+from text_summarize.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/text_summarize/pipeline/stage_data_ingestion.py
@@ -0,0 +1,15 @@
+from text_summarize.config.configuration import ConfigurationManager
+from text_summarize.components.data_ingestion import DataIngestion
+from text_summarize.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
