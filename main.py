838f69ac249711ecc9514c504dda983af495378a
mazen <mazenchouchene81@gmail.com>
2024-06-26 13:36:29 +0200

    data validation completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from text_summarize.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
+from text_summarize.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+from text_summarize.pipeline.stage2_data_validation import DataValidationTrainingPipeline
 
 from text_summarize.logging import logger
 
@@ -7,6 +8,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/text_summarize/entity/__init__.py
+++ src/text_summarize/entity/__init__.py
@@ -8,3 +8,16 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file:Path
     unzip_dir:Path
+
+
+
+#entity
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
